56ab23333362dbe10a4afaca36ceae961c28a3d6
Bruno Vigel <brunorosav@gmail.com>
2022-04-08 19:54:07 -0300

    correcao hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,7 +8,7 @@ export function Hero() {
       <div className="hero">
         <Header />
         <div className="container">
-          <h1><Typewriter words={['Olá, eu sou o Bruno Vigel']} cursor cursorStyle='|' /></h1>
+          <h1><Typewriter words={['Olá, eu sou o Bruno']} cursor cursorStyle='|' /></h1>
           <p>
             Olá! Meu nome é Bruno, tenho 22 anos e sou desenvolvedor FrontEnd.
             Apaixonado por aprender novas tecnologias e técnicas, pensando na
