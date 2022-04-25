1e211eacbc5e3eabf31bb3117fab82b4625eab96
Bruno Vigel <brunorosav@gmail.com>
2022-04-25 18:10:18 -0300

    teste

--- src/Pages/Contato/index.jsx
+++ src/Pages/Contato/index.jsx
@@ -1,11 +1,9 @@
 import { Header } from "../../Components/Header";
-import {Footer} from '../../Components/Footer'
 
 export function Contato() {
     return (
         <>
             <Header />
-            <Footer />
         </>
     )
 }
