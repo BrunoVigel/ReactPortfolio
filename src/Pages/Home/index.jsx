837901db8a0fcfca35a7d995a3bb120c64f41b8f
Bruno Vigel <brunorosav@gmail.com>
2022-04-25 13:32:43 -0300

    test 2 fix 5 footer

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import {Hero} from '../../Components/Hero'
+
 import {Projetos} from '../../Components/Projetos'
 import {Skills} from '../../Components/Skills'
 import {Footer} from '../../Components/Footer'
@@ -8,7 +8,7 @@ import {Footer} from '../../Components/Footer'
 export function Home() {
     return(
         <>
-            <Hero />
+
             <Projetos />
             <Skills />
             <Footer />
