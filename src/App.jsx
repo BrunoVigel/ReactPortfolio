d814f2ba8130f0a782589704a60a098d70f5ea58
Bruno Vigel <brunorosav@gmail.com>
2022-04-25 18:18:13 -0300

    teste 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {Home} from './Pages/Home'
 import { Contato } from './Pages/Contato'
 import { SkillsPage } from './Pages/SkillsPage'
 import { Projetos } from './Pages/Projetos'
-import {Footer} from './Components/Footer'
+import {Footer} from './Components/footer'
 
 function App() {
 
