1d3601ecdc012742f7bccb2c65b7e0f24a4109b0
Bruno Vigel <brunorosav@gmail.com>
2022-04-25 21:03:40 -0300

    teste 12

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import './App.css'
 // import {BrowserRouter, Routes, Route} from 'react-router-dom'
 // import {Home} from './Pages/Home'
 // import { Contato } from './Pages/Contato'
-import { SkillsPage } from './Pages/SkillsPage'
+// import { SkillsPage } from './Pages/SkillsPage'
 // import { Projetos } from './Pages/Projetos'
-// import {Footer} from './Components/footer'
+import {Footer} from './Components/Footer'
 
 function App() {
 
   return (
     <div className="App">
-      <SkillsPage />
+      
+      <Footer />
     </div>
   )
 }
