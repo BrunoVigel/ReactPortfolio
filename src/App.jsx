4eb9914d8810749646eb9e73127159b00a745e3f
Bruno Vigel <brunorosav@gmail.com>
2022-04-25 21:12:37 -0300

    teste 16

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
-// import {BrowserRouter, Routes, Route} from 'react-router-dom'
-// import {Home} from './Pages/Home'
-// import { Contato } from './Pages/Contato'
-// import { SkillsPage } from './Pages/SkillsPage'
-// import { Projetos } from './Pages/Projetos'
-import {Footer} from './components/Footer'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Home} from './Pages/Home'
+import { Contato } from './Pages/Contato'
+import { SkillsPage } from './Pages/SkillsPage'
+import { Projetos } from './Pages/Projetos'
+import { Footer } from './components/Footer'
 
 function App() {
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contato' element={<Contato />} />
+          <Route path='/skills' element={<SkillsPage />} />
+          <Route path='/projetos' element={<Projetos />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
       
-      <Footer />
     </div>
   )
 }
